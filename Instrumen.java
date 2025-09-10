72b6ada873dfc6c5e5b3ce4d042fbeceed85fc9d
azraayasminn <azraayasminns@gmail.com>
2025-09-10 09:24:14 +0700

    Add files via upload

--- Instrumen.java
+++ Instrumen.java
@@ -6,8 +6,28 @@ public class Instrumen {
         name = "Guittar";
         price = 175000;
     }
+    public String getname(){
+        return name;
+    }
+    public double getprice(){
+        return price;
+    }
+    public void setname(String n){
+        name = n;
+    }
+    public void setprice(double p){
+        price = p;
+    }
+
+
     public void print(){
         System.out.println("Name: "+name);
         System.out.println("Price: "+price);
     }
+
+    public Instrumen(String n, double p){
+        name = n;
+        price = p;  
+    }
+    
 }
--- InstrumenTest.java
+++ InstrumenTest.java
@@ -1,7 +1,12 @@
 public class InstrumenTest {
     public static void main(String[] args) {
         Instrumen guitar = new Instrumen();
+        Instrumen piano = new Instrumen ("Piano", 160000);
+        Instrumen drum = new Instrumen("Drum", 500000);
+
 
         guitar.print();
+        piano.print();
+        drum.print();
     }
 }
